24e17773d7b152ed80582df37198d839093227b0
Erjon-19 <49555420+Erjon-19@users.noreply.github.com>
2019-09-19 20:17:20 +0300

    Add files via upload
    event.addListener στον χρήστη

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ date_default_timezone_set('Europe/Athens'); //set the default time zone used by 
 <div class="container"> 
 <div class=row>
 <div class=col-md-3>
-	 <IMG SRC="logo.png" ALT="some text" WIDTH=250 HEIGHT=110>
+	 <IMG SRC="logo.png" ALT="some text" WIDTH=240 HEIGHT=100>
 		
 		<a href='admin/index.php'><button class='btn btn-primary' style='width:200px;'>Admin page</button></a>
 		
@@ -54,10 +54,9 @@ date_default_timezone_set('Europe/Athens'); //set the default time zone used by 
 	<div id=map style='width:100% ; height:600px'></div>
 	
 	 <br><br>
+
    <script>
 
-
-		
       function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 13,
@@ -69,71 +68,68 @@ date_default_timezone_set('Europe/Athens'); //set the default time zone used by 
 		 
 		<?php
 		
-		$q=mysqli_query($cn,"select * from polygon");
-		
-		
-		while($r=mysqli_fetch_array($q))
-		{
-			if($r['coords']!="")
-			{
-					$ALLcoordinates= explode(" ",$r['coords']);
-					$vale_coma=0;
-					
-					echo "var coords$r[id] = [";
-					foreach ($ALLcoordinates as $i_coord)
-					{
-						if($vale_coma==1) echo ",";
-						$vale_coma=1;
-						
-						$xy=explode(",",$i_coord);
-						echo "{lat:$xy[1], lng:$xy[0]}";
-						
-					}
-					
-					
-					echo "];
-					
-	
-					 
-					 polygono$r[id] = new google.maps.Polygon({
-						  paths: coords$r[id],
-						  fillColor: '#888'
-					});
-				polygono$r[id].setMap(map);
-				
-
+			$q=mysqli_query($cn,"select * from polygon");
 			
+			while($r=mysqli_fetch_array($q))
+			{
+				if($r['coords']!="")
+				{
+						$ALLcoordinates= explode(" ",$r['coords']);
+						$vale_coma=0;
+						
+						echo "var coords$r[id] = [";
+						foreach ($ALLcoordinates as $i_coord)
+						{
+							if($vale_coma==1) echo ",";
+							$vale_coma=1;
+							
+							$xy=explode(",",$i_coord);
+							echo "{lat:$xy[1], lng:$xy[0]}";
+							
+						}
+						
+						
+						 echo "];
+						
+						polygono$r[id] = new google.maps.Polygon({
+							paths: coords$r[id],
+							fillColor: '#888'
+						});
+					polygono$r[id].setMap(map);
+					
+					google.maps.event.addListener(polygono$r[id], 'click', function (event) {
 					
 					
+						var inf=   new google.maps.InfoWindow({
+			  content: \"Polulation: $r[population]<br>Places Parking: $r[places]<br><a href='edit_sq.php?id=$r[id]'>βαλε τα στοιχεια</a>\"
+			});
+						inf.setPosition(event.latLng);
+						 inf.open(map);
+							
+						 
+						});
+
+
+					"; 
+				} 
+
 				
 				
-				
-				";
+		
 			}
 			
-	
-		}
-		
-	
-		
-		$js=file_get_contents("http://localhost/admin/json.php?tm=".$tm);
-		
-		$j=json_decode($js);
-		 
-		  foreach ($j->res as $rec)
-		  {
-			 
-			  $id=$rec->id;
-			  $p=$rec->p;
-			  if($p<0.59) echo " polygono$id.setOptions({ fillColor: 'green'});";
-			  if($p>=0.59 && $p<0.84) echo " polygono$id.setOptions({ fillColor: 'yellow'});";
-			  if($p>=0.84)	echo " polygono$id.setOptions({ fillColor: 'red'});";
-		  
-			  
-		  }
-		  
-		 
-		
+			$js=file_get_contents("http://localhost/admin/json.php?tm=".$tm);
+			$j=json_decode($js);
+			
+			foreach ($j->res as $rec)
+			{
+				
+				$id=$rec->id;
+				$p=$rec->p;
+				if($p<0.59) echo " polygono$id.setOptions({ fillColor: 'green'});";
+				if($p>=0.59 && $p<0.84) echo " polygono$id.setOptions({ fillColor: 'yellow'});";
+				if($p>=0.84)	echo " polygono$id.setOptions({ fillColor: 'red'});";  
+			}	
 		
 		?>
       } 
@@ -142,7 +138,7 @@ date_default_timezone_set('Europe/Athens'); //set the default time zone used by 
 
 	<!--The key for google APIs, and at the end of ./body for efficient load javascript with async -->
     <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDxxklFcdKu0AEvC4qcf1ffW-KK7zocdDE&callback=initMap">
+    	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDxxklFcdKu0AEvC4qcf1ffW-KK7zocdDE&callback=initMap">
     </script>
 
 </div>
